66ecc7ae06a6cdb69ac2dd765d89d466d9a05eb8
Joshiboy95 <39739189+Joshiboy95@users.noreply.github.com>
2024-05-23 16:42:28 +0200

    added scroll animation

--- js/script.js
+++ js/script.js
@@ -151,6 +151,24 @@ function navigateDatenschutz() {
 }
 
 
+// ================================= Scroll in Viewport animation =================================
+
+$(document).on("scroll", function() {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var tags = $(".scroll-fade-in"); //war mal ".tag"
+
+  var width = (window.innerWidth > 0) ? window.innerWidth : screen.width; // Device screen size
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i];
+    if ($(tag).position().top < pageBottom) {
+      $(tag).addClass("visible");
+    } else {
+      //$(tag).removeClass("visible"); //hiermit animiert es immer wieder
+    }
+  }
+});
 
 
 
