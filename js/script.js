9d14e66b75c5875dc19f4a7709fc3dca5b1130de
eliasthoma <elias.thoma@hotmail.de>
2024-05-24 21:03:10 +0200

    disabled scroll animation, which is not working correctly

--- js/script.js
+++ js/script.js
@@ -76,22 +76,22 @@ function changeBtnColor() {
 
 // ================================= Scroll in Viewport animation =================================
 
-$(document).on("scroll", function() {
-  var pageTop = $(document).scrollTop();
-  var pageBottom = pageTop + $(window).height();
-  var tags = $(".scroll-fade-in"); //war mal ".tag"
+// $(document).on("scroll", function() {
+//   var pageTop = $(document).scrollTop();
+//   var pageBottom = pageTop + $(window).height();
+//   var tags = $(".scroll-fade-in"); //war mal ".tag"
 
-  var width = (window.innerWidth > 0) ? window.innerWidth : screen.width; // Device screen size
+//   var width = (window.innerWidth > 0) ? window.innerWidth : screen.width; // Device screen size
 
-  for (var i = 0; i < tags.length; i++) {
-    var tag = tags[i];
-    if ($(tag).position().top < pageBottom) {
-      $(tag).addClass("visible");
-    } else {
-      //$(tag).removeClass("visible"); //hiermit animiert es immer wieder
-    }
-  }
-});
+//   for (var i = 0; i < tags.length; i++) {
+//     var tag = tags[i];
+//     if ($(tag).position().top < pageBottom) {
+//       $(tag).addClass("visible");
+//     } else {
+//       //$(tag).removeClass("visible"); //hiermit animiert es immer wieder
+//     }
+//   }
+// });
 
 
 
